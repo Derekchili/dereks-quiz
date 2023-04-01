d357549e62a8e090d34690056668212231c9277b
derek <derekchili@gmail.com>
2023-04-01 16:46:50 -0700

    updates

--- script.js
+++ script.js
@@ -5,90 +5,139 @@ var highScoreH2 = document.querySelector("#highscore");
 // variable to start the game
 var startBtn = document.querySelector("#start-time");
 // variable for questions multiple choice
-var  multipleUl= document.querySelector("#MultipleChoice");
+var  multiple1= document.querySelector(".ans1");
+var  multiple2= document.querySelector(".ans2");
+var  multiple3= document.querySelector(".ans3");
+var  multiple4= document.querySelector(".ans4");
 // variable to keep initials with highscore
 var intials = document.querySelector("#initials");
 // defines variable for questions
 var qBox = document.querySelector("#questions-box");
+var holdInterval = 0;
+// this variable stores the penalty time for wrong answer
+var penalty = 10;
+// define all questions and answers asked BCS can have mutiple things here
+var Qh1 = document.querySelector("h1");
 
-var questions1 = [{
-    ask:"How many eyelids do dogs have?",
-    mulitple:["3", "5","2", "1",],
-    answer:"3" }]
+var allQuestions = {
+    q1 :{ question1:"How many eyelids do dogs have?",
+          answers1:["3", "5", "2", "1"],
+          correct1:"3",
+        },
+    
+    q2 :{ question2:"What sense are puppies born without?",
+          answers2:["eyesight", "taste", "hearing", "smell"],
+          correct2:"hearing",
+         },
+    
+    q3 :{ question3:"What is the most popular breed of dog in the USA?",
+          answers3:["Poodle", "German Shepard", "Labrador", "Golden Retriever"],
+          correct3:"Labrador",
+        },
+    
+    q3 :{ question4:"How many teeth do adult dogs have?",
+          answers4:["36", "42", "54", "28"],
+          correct4:"42",
+      },
 
-var question2 = [{
-    ask:"What sense are puppies born without?",
-    multiple:["eyesight", "taste", "hearing", "smell"],
-    answer:"hearing"}]
+};
 
-var question3 = [{
-    ask:"What is the most popular breed of dog in the USA?",
-    multiple:["Poodle", "German Shepard", "Labrador", "Golden Retriever"],
-    answer:"Labrador"}]
 
-var question4 = [{
-    ask:"How many teeth do adult dogs have?",
-    multiple:["36", "42", "54", "28"],
-    answer:"42"}];
 
-var quizQ = ["questions1", "question2", "question3", "question4"];
-var randomQ = getRandomItems(quizQ);
+//  random questions function I think.....
 
-function getRandomItems(quizQ) {
-    let usedIndexes = []; //keeps track of used index
-    let randomIitems = [];
 
-    for (let i = 0; i < quizQ.length; i++) {
-        let randomIndex;
+// function getRandomItems(allQuestions) {
+//     let usedIndexes = []; //keeps track of used index
+//     let randomIitems = [];
 
-        do {
-            randomIndex = Math.floor(Math.random() * quizQ.length); //generates a random index
-        } while (usedIndexes.includes(randomIndex)); // makes sure the index has not been used yet
-        usedIndexes.push(randomIndex); //add index to the usedIndexes
-        randomIitems.push(quizQ[randomIndex]);
-    }
-    return randomIitems;
-}
-console.log(randomQ);
-//  random questions function
-// var questionIndex = 0;
+//     for (let i = 0; i < allQuestions.length; i++) {
+//         let randomIndex;
 
-//     function quiz() {
-//         for (var i = 0; userChoice > userQuestion; i++) {
-//                 var userQuestion = questions[questionIndex].ask;
-//                 var userChoice = questions[questionIndex].multiple;
-//         }   
-//             console.log(userQuestion);
-//         }
-// quiz()
+//         do {
+//             randomIndex = Math.floor(Math.random() * allQuestions.length); //generates a random index
+//         } while (usedIndexes.includes(randomIndex)); // makes sure the index has not been used yet
+//         usedIndexes.push(randomIndex); //add index to the usedIndexes
+//         return allQuestions[randomIndex];
+//         // randomIitems.push(quizQ[randomIndex]);
+//     }
+//     return randomIitems;
+// }
+// console.log(allQuestions);
+
+
+
 // hids the container
 qBox.style.display = "none";
 
 var secondsLeft = 60;
 // this function sets the  the time viarable
-function startTime() {
-    var timeStarter = setInterval(function(){    secondsLeft--;
-        timeH2.textContent = secondsLeft;
-        if(secondsLeft === 0) {
-          clearInterval(timeStarter);  
-        }},1000)
-}
-startTime();
-
-
-var holdInterval = 0;
-// this variable stores the penalty time for wrong answer
-var penalty = 10;
-// define all questions and answers asked BCS can have mutiple things here
-
 // function that starts the game/timer?
+function startTime() {
+    startBtn.addEventListener("click", function(){
+    questionOne();
+    startBtn.style.display = "none";
+        var timeStarter = setInterval(function(){    
+        secondsLeft--;
+        timeH2.textContent = secondsLeft + " seconds left";
+        if(secondsLeft => 0) {
+          clearInterval(timeStarter);  
+          clearInterval(startBtn)
+        }},1000)
+        
+})
+};
+startTime();
+   
+function questionOne() {
+    Qh1.textContent = allQuestions.q1.question1;
+// // repeat more times
+    multiple1.textContent = allQuestions.q1.answers1[0];
+    multiple2.textContent = allQuestions.q1.answers1[1];
+    multiple3.textContent = allQuestions.q1.answers1[2];
+    multiple4.textContent = allQuestions.q1.answers1[3];
 
+//     // Qh1.textContent = allQuestions.q2.question2;
+
+//     multiple1.textContent = allQuestions.q2.answers2[0];
+//     multiple2.textContent = allQuestions.q2.answers2[1];
+//     multiple3.textContent = allQuestions.q2.answers2[2];
+//     multiple4.textContent = allQuestions.q2.answers2[3];
+
+    // Qh1.textContent = allQuestions.q3.question3;
+
+    // multiple1.textContent = allQuestions.q3.answers3[0];
+    // multiple2.textContent = allQuestions.q3.answers3[1];
+    // multiple3.textContent = allQuestions.q3.answers3[2];
+    // multiple4.textContent = allQuestions.q3.answers3[3];
+
+    // Qh1.textContent = allQuestions.q4.question4;
+
+    // multiple1.textContent = allQuestions.q4.answers4[0];
+    // multiple2.textContent = allQuestions.q4.answers4[1];
+    // multiple3.textContent = allQuestions.q4.answers4[2];
+    // multiple4.textContent = allQuestions.q4.answers4[3];
+    
+
+};
+
+// have one for the right answers under here
+    // multiple1.textContent = allQuestions.q1.correct1;
+    // multiple2.textContent = allQuestions.q2.correct2;
+    // multiple3.textContent = allQuestions.q4.correct3;
+    // multiple4.textContent = allQuestions.q4.correct4;
+
+
+
+
+// })
 //      start quiz
 //          hide start button
 //          show the quiz container
 //          display first question
 //          start timer
 //          display countdown on screen
+
 // I'll need to append the child or element in html
 // function not sure if this will work that will start the questions
 // var questionIndex = 0;
@@ -158,3 +207,4 @@ var penalty = 10;
 //      click answers
 //      click save score
 //      keyups
+// 
